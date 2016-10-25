417eb751a6303afe2cfbe28bbbf5c96d6065944d
David Baucum <maxolasersquad@gmail.com>
2016-10-25 11:31:32 -0400

    Get the content-type from the wirter.
    The TypedWriterInterface class requires the getDefaultMimeType method to be defined. This method returns the proper content-type.

--- Exporter/Exporter.php
+++ Exporter/Exporter.php
@@ -43,19 +43,15 @@ class Exporter
         switch ($format) {
             case 'xls':
                 $writer = new XlsWriter('php://output');
-                $contentType = 'application/vnd.ms-excel';
                 break;
             case 'xml':
                 $writer = new XmlWriter('php://output');
-                $contentType = 'text/xml';
                 break;
             case 'json':
                 $writer = new JsonWriter('php://output');
-                $contentType = 'application/json';
                 break;
             case 'csv':
                 $writer = new CsvWriter('php://output', ',', '"', '', true, true);
-                $contentType = 'text/csv';
                 break;
             default:
                 throw new \RuntimeException('Invalid format');
@@ -67,7 +63,7 @@ class Exporter
         };
 
         return new StreamedResponse($callback, 200, array(
-            'Content-Type' => $contentType,
+            'Content-Type' => $writer->getDefaultMimeType(),
             'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
         ));
     }
